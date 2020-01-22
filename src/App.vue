15a9728fdafbf2b9a72e2492ce0fd4d4d2686987
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2020-01-22 06:06:38 -0300

    Mais Ajustes

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,93 @@
 <template>
-	<div>
-		<div id="slogan" class="text-center">
-			<h1>Pagina Principal</h1>
-			<br />
-			<h6 class="text-secondary">Gerador de nomes ultilizando Vue.Js, GraphQl e Node</h6>
-		</div>
-		<div id="main">
-			<div class="container">
-				<div class="row">
-					<div class="col-md">
-						<h5>
-							Prefixos
-							<span class="badge badge-info">{{prefixes.length}}</span>
-						</h5>
-						<div class="card">
-							<div class="card-body">
-								<ul class="list-group">
-									<li class="list-group-item" v-for="prefixe in prefixes" v-bind:key="prefixe">{{prefixe}}</li>
-								</ul>
-								<br />
-								<div class="input-group">
-									<input class="form-control" type="text" placeholder="Digite o Prefixo" />
-									<div class="input-group-append">
-										<button class="btn btn-info">
-											<span class="fa fa-plus"></span>
-										</button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md">
-						<h5>
-							Sufixos
-							<span class="badge badge-info">{{ sufixes.length}}</span>
-						</h5>
-						<div class="card">
-							<div class="card-body">
-								<ul class="list-group">
-									<li class="list-group-item" v-for="sufixe in sufixes" v-bind:key="sufixe">{{ sufixe}}</li>
-								</ul>
-								<br />
-								<div class="input-group">
-									<input class="form-control" type="text" placeholder="Digite o Sufixo" />
-									<div class="input-group-append">
-										<button class="btn btn-info">
-											<span class="fa fa-plus"></span>
-										</button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<br />
-				<h5>
-					Dominios
-					<span class="badge badge-info">{{ domains.length}}</span>
-				</h5>
-				<div class="card">
-					<div class="card-body">
-						<ul class="list-group">
-							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{ domain }}</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div>
+        <div id="slogan" class="text-center">
+            <h1>Pagina Principal</h1>
+            <br />
+            <h6 class="text-secondary">Gerador de nomes ultilizando Vue.Js, GraphQl e Node</h6>
+        </div>
+        <div id="main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md">
+                        <h5>
+                            Prefixos
+                            <span class="badge badge-info">{{prefixes.length}}</span>
+                        </h5>
+                        <div class="card">
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li
+                                        class="list-group-item"
+                                        v-for="prefixe in prefixes"
+                                        v-bind:key="prefixe"
+                                    >{{prefixe}}</li>
+                                </ul>
+                                <br />
+                                <div class="input-group">
+                                    <input
+                                        class="form-control"
+                                        type="text"
+                                        placeholder="Digite o Prefixo"
+                                    />
+                                    <div class="input-group-append">
+                                        <button class="btn btn-info" v-on:click="addPrefix()">
+                                            <span class="fa fa-plus"></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <h5>
+                            Sufixos
+                            <span class="badge badge-info">{{ sufixes.length}}</span>
+                        </h5>
+                        <div class="card">
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li
+                                        class="list-group-item"
+                                        v-for="sufixe in sufixes"
+                                        v-bind:key="sufixe"
+                                    >{{ sufixe}}</li>
+                                </ul>
+                                <br />
+                                <div class="input-group">
+                                    <input
+                                        class="form-control"
+                                        type="text"
+                                        placeholder="Digite o Sufixo"
+                                    />
+                                    <div class="input-group-append">
+                                        <button class="btn btn-info" v-on:click="addSufix()">
+                                            <span class="fa fa-plus"></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <h5>
+                    Dominios
+                    <span class="badge badge-info">{{ domains.length}}</span>
+                </h5>
+                <div class="card">
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li
+                                class="list-group-item"
+                                v-for="domain in domains"
+                                v-bind:key="domain"
+                            >{{ domain }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -75,28 +95,28 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
 export default {
-	name: "app",
-	data() {
-		return {
-			prefixes: ["air", "ana"],
-			sufixes: ["bar", "paula"],
-			domains: ["teste", "teste2", "teste3", "teste4", "teste5"]
-		};
-	}
+    name: "app",
+    data() {
+        return {
+            prefixes: ["air", "ana", "paulo", "Jorge"],
+            sufixes: ["bar", "paula", "henrique", "teste"],
+            domains: ["teste", "teste2", "teste3", "teste4", "teste5"]
+        };
+    }
 };
 </script>
 
 <style>
 #slogan {
-	margin-top: 30px;
-	margin-bottom: 30px;
+    margin-top: 30px;
+    margin-bottom: 30px;
 }
 
 #main {
-	background-color: #c2bbbb;
-	padding: 30px;
-	width: 100%;
-	max-width: 800px;
-	margin: 0 auto;
+    background-color: #c2bbbb;
+    padding: 30px;
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
 }
 </style>
